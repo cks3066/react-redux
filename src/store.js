5053b9ab110dacffa05fc2665cea83d6963af95a
On <cks3066@naver.com>
2021-09-10 02:30:59 +0900

    리액트 리덕스 - createSlice 적용 (제일 간단한 방법)

--- src/store.js
+++ src/store.js
@@ -1,57 +1,22 @@
-import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
-// 액션
-const ADD = "ADD";
-const DELETE = "DELETE";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-// 액션 크리에이터
-const addToDo = createAction(ADD);
-// const addToDo = (text) => {
-//   return {
-//     type: ADD,
-//     text,
-//   };
-// };
-
-const deleteToDo = createAction(DELETE);
-// const deleteToDo = (id) => {
-//   return {
-//     type: DELETE,
-//     id: parseInt(id),
-//   };
-// };
-
-// 리듀서
-const reducer = createReducer(
-  [
-    /*initialState*/
-  ],
-  {
-    [ADD]: (state, action) => {
+// 크리에이트 슬라이트 = 액션  + 리듀서
+const toDos = createSlice({
+  name: "toDoReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
       state.push({ text: action.payload, id: Date.now() });
     },
-    [DELETE]: (state, action) => {
+    remove: (state, action) => {
       state.filter((toDo) => toDo.id !== action.payload);
     },
-  }
-);
-// const reducer = (state = ["hello"], action) => {
-//   switch (action.type) {
-//     case ADD:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case DELETE:
-//       return state.filter((toDo) => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
+  },
+});
 
 // 스토어
-const store = configureStore({ reducer });
-// const store = createStore(reducer);
+const store = configureStore({ reducer: toDos.reducer });
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+export const { add, remove } = toDos.actions;
 
 export default store;
