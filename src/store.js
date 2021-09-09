a1184a77e9308ff7366b20f14b18e77cf22e9219
On <cks3066@naver.com>
2021-09-10 02:19:10 +0900

    리액트 리덕스 툴킷 사용 createAction, createReducer, configureStore

--- src/store.js
+++ src/store.js
@@ -1,38 +1,53 @@
-import { createStore } from "redux";
-
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 // 액션
 const ADD = "ADD";
 const DELETE = "DELETE";
 
 // 액션 크리에이터
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+const addToDo = createAction(ADD);
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+const deleteToDo = createAction(DELETE);
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id: parseInt(id),
+//   };
+// };
 
 // 리듀서
-const reducer = (state = ["hello"], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
+const reducer = createReducer(
+  [
+    /*initialState*/
+  ],
+  {
+    [ADD]: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    [DELETE]: (state, action) => {
+      state.filter((toDo) => toDo.id !== action.payload);
+    },
   }
-};
+);
+// const reducer = (state = ["hello"], action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case DELETE:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
 
 // 스토어
-const store = createStore(reducer);
+const store = configureStore({ reducer });
+// const store = createStore(reducer);
 
 export const actionCreators = {
   addToDo,
